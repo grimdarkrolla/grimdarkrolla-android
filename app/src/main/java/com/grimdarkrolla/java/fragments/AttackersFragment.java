75c9f0f53e5c52f09f64a150a86abdedcd8ac664
Evan Slaton <evanslaton@gmail.com>
2019-02-18 11:08:04 -0800

    Added fragment code to MainActivity

---
+++ app/src/main/java/com/grimdarkrolla/java/fragments/AttackersFragment.java
@@ -0,0 +1,18 @@
+package com.grimdarkrolla.java.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.grimdarkrolla.java.R;
+
+public class AttackersFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.attackers_view, container, false);
+    }
+}
